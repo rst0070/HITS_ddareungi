64199e6d445a097c5a4de5df9f2ec5add242c2e3
Wonbin Kim <kwb0711@gmail.com>
2024-04-26 23:31:07 +0900

    encoding problems

--- data/file_encoding.py
+++ data/file_encoding.py
@@ -1,9 +1,9 @@
 eur_kr =  open("./tpss_bcycl_od_statnhm_20240421.csv", mode="r", encoding="euc-kr")
-utf_8 = open("./20240421.csv", mode="w")
+utf_8 = open("./20240421.csv",encoding='utf-8', mode="w")
 
-for line in eur_kr:
-    utf_8.write(line.encode("utf-8"))
-    
+content = eur_kr.read()
+utf_8.write(content)
+
 eur_kr.close()
 utf_8.close()
     
