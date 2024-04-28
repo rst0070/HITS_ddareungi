7f27a10e7523b6d09abf2d8c7d150d2760c13a94
rst0070 <52444137+rst0070@users.noreply.github.com>
2024-04-28 17:13:10 +0900

    HITS

--- data/main.py
+++ data/main.py
@@ -8,48 +8,61 @@ from tqdm import trange
 ## RDD is like below
 ## (start stop, [(end stop, number of bikes used that route), ....])
 
-def train(
+def calculate(
     max_epoch:int,
     A:Dict[str, List[Tuple[str, float]]], 
     A_t:Dict[str, List[Tuple[str, float]]], 
     h_score:Dict[str, float], 
     a_score:Dict[str, float]):
     
-    pbar = trange(0, max_epoch, desc="training")
+    pbar = trange(0, max_epoch, desc="calculating score vectors")
     for epoch in pbar:
         
         ## update h_score
+        max_val_of_vec = 1e-12
         for hub, out_links in A.items():
             score_sum = 0. # for hub node
             for authority, rate_link in out_links:
                 score_sum += rate_link * a_score[authority]
-            #print(score_sum)
+            
+            max_val_of_vec = max(max_val_of_vec, score_sum)
             h_score[hub] = score_sum
         
+        ## normalize h_score
+        for hub in h_score.keys():
+            h_score[hub] /= max_val_of_vec
+        
+        
         ## update a_score
+        max_val_of_vec = 1e-12
         for authority, in_links in A_t.items():
             score_sum = 0. # for authority node
             for hub, rate_link in in_links:
                 score_sum += rate_link * h_score[hub]
                 
+            max_val_of_vec = max(max_val_of_vec, score_sum)
             a_score[authority] = score_sum
-    
-    # pbar = trange(0, max_epoch, desc="node ranking")
-    # for epoch in pbar:
-        
-    #     ## update a_score
-    #     for authority, in_links in A_t.items():
-    #         score_sum = 0. # for authority node
-    #         for hub, rate_link in in_links:
-    #             score_sum += rate_link * a_score[hub]
-                
-    #         a_score[authority] = score_sum
+            
+        for authority in a_score.keys():
+            a_score[authority] /= max_val_of_vec
     
     #print(h_score)
 
+def saveScoreVectorAsCsv(data:Dict[str, float], path:str):
+    with open(path, 'w') as f:
+        f.write("stop_id,score\n")
+        for node, score in data.items():
+            f.write(f"{node},{score}\n")
+        f.close()
 
 
-def main(train_epoch:int, link_data_path:str, id_data_path:str):
+
+def main(
+    train_epoch:int,
+    link_data_path:str,
+    id_data_path:str,
+    save_h_path:str,
+    save_a_path:str):
     
     spark = SparkSession \
     .builder \
@@ -70,24 +83,24 @@ def main(train_epoch:int, link_data_path:str, id_data_path:str):
     h_score = h_score.collectAsMap()
     a_score = deepcopy(h_score)
     
-    train(
+    calculate(
         max_epoch=train_epoch,
         A=A,
         A_t=A_t,
         h_score=h_score,
         a_score=a_score
     )
-    #print(h_score[min(h_score, key=h_score.get)])
-    print("------------------------------")
-    print(a_score)
-    #print(A_t)
-    #print(h_score)
+    
+    saveScoreVectorAsCsv(a_score, save_a_path)
+    saveScoreVectorAsCsv(h_score, save_h_path)
     
     
 if __name__ == "__main__":
     main(
-        train_epoch=1000,
+        train_epoch=10000,
         link_data_path='./encoded_data/20240421.csv',
-        id_data_path='./encoded_data/stops.csv'
+        id_data_path='./encoded_data/stops.csv',
+        save_h_path="./hub_score/20240421.csv",
+        save_a_path="./authority_score/20240421.csv"
     )
 
