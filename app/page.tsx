419d5562664a52a1804746498eea0eb0b99cf10e
Wonbin Kim <kwb0711@gmail.com>
2024-05-09 01:10:07 +0900

    .

--- app/page.tsx
+++ app/page.tsx
@@ -15,11 +15,25 @@ function createStopElement(stop_info:StopInfo){
   )
 }
 
+function createStopList(stop_list:StopInfo[]){
+  return (
+    <div className="scroll">
+      {
+        stop_list.map(createStopElement)
+      }
+    </div>
+    
+  )
+}
+
 const hubElements:JSX.IntrinsicElements['div'][] = []
 for(let i = 0; i < HScore.length; i++)
   hubElements.push( createStopElement(HScore[i]) )
 
 export default function Home() {
+  let HList = createStopList(HScore)
+  let AList = createStopList(AScore)
+
   return (
     <>
       <NavBar/>
@@ -29,9 +43,15 @@ export default function Home() {
             <KakaoMap />
           </section>
           <aside className="grid-item">
-            <div className="scroll">
-            {hubElements}
-            </div>
+            {HList}
+          </aside>
+        </div>
+        <div className="grid-container">
+          <section className="grid-item">
+            <KakaoMap />
+          </section>
+          <aside className="grid-item">
+            {AList}
           </aside>
         </div>
       </center>      
--- components/KakaoMap.tsx
+++ components/KakaoMap.tsx
@@ -1,6 +1,6 @@
 import Script from 'next/script'
 import React from 'react'
-import {Map} from 'react-kakao-maps-sdk'
+import {Map, MapMarker} from 'react-kakao-maps-sdk'
 
 const APP_KEY = '633117026be1caa09108a7721bed0b2d'
 const KAKAO_SDK_URL = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${APP_KEY}&autoload=false`;
@@ -14,7 +14,15 @@ const KakaoMap
       <Map 
         center={{ lat: START_POSITION.latitude, lng: START_POSITION.longitude }}
         style={{ width: '100%', height: '100%' }} 
+      >
+        <MapMarker // 마커를 생성합니다
+        position={{
+          // 마커가 표시될 위치입니다
+          lat: START_POSITION.latitude,
+          lng: START_POSITION.longitude,
+        }}
       />
+      </Map>
     </div>
   );
 };
