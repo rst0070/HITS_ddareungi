4d285ac4cc61aad3e6b29056be77d83d2618d102
Wonbin Kim <kwb0711@gmail.com>
2024-05-10 02:07:48 +0900

    github setting

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/HITS_ddareungi",
+    output: "export",
+    reactStrictMode: true,
+  };
 
 export default nextConfig;
