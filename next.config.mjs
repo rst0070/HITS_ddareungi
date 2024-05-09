d0968797b8dbe92ce52fbf0639ee99e7a5a9807a
Wonbin Kim <kwb0711@gmail.com>
2024-05-10 02:31:17 +0900

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const prefix = process.env.NODE_ENV === 'production' ? 'https://rst0070.github.io/HITS_ddareungi/' : ''
+
+const nextConfig = {
+    output: 'export',
+    assetPrefix: prefix
+};
 
 export default nextConfig;
